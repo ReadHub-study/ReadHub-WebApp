f2e280b1b1aeab4c54d7c1a62a9a4db11f2c96a0
Emiju-Samuel <samuelemiju@gmail.com>
2026-03-11 15:11:12 -0700

    Fixing error in previous commit

--- ReadHub_Frontend/src/Pages/Auth/Login.jsx
+++ ReadHub_Frontend/src/Pages/Auth/Login.jsx
@@ -68,7 +68,7 @@ const Login = () => {
       if (response.status === 200) {
         toast.success("Logged in successfully.");
         localStorage.setItem("accessToken", response.data.accessToken);
-        navigate("/");
+        navigate("/home");
       }
     } catch (err) {
       setError(
