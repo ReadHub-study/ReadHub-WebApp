6228456de411b39a1d08f87c272ebce4bf3432de
Lucian Codes <ayomideayomipo147@gmail.com>
2026-03-17 23:45:19 +0100

    fixes

--- Backend/controllers/notes.controller.js
+++ Backend/controllers/notes.controller.js
@@ -90,6 +90,7 @@ export const getAllNotes = async (req, res) => {
     return res.status(500).json({ message: error.message })
   }
 }
+// get note by id
 
 export const getNoteById = async (req, res) => {
   try {
